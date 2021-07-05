1648319694a6c3cd9baa567260bbec8c85af106e
Victoria Tejeda <vicyatm@gmail.com>
2021-07-05 00:11:58 -0500

    Interfaz basica funcional, valida numero y enmascara caracteres

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import validator from './validator.js';
 
-console.log(validator);
+window.validaNum = function validaNum(event){
+    if(event.charCode >= 48 && event.charCode <= 57){
+        return true;
+       }
+       return false; 
+}
+
+window.validaBtn = function validaBtn(){
+    let ccNumber= document.getElementById("CardNum").value;
+    if(ccNumber.length ==16){
+        validator.maskify(ccNumber);
+        validator.isValid(ccNumber);
+        if (validator.isValid(ccNumber)){
+            document.getElementById("mask").innerHTML = "El número de tu tarjeta es: " + validator.maskify(ccNumber);
+        }else {
+            alert("ingresa un número valido");
+        }
+    }else{
+        alert("ingresa los 16 digitos de tu tarjeta");
+    }
+}
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,72 @@
 const validator = {
-  // ...
+  isValid(ccNumber) {
+    let result = [];
+    if (ccNumber) {
+      let cardReverse = reverse(ccNumber);
+      //console.log(cardReverse);
+      for (let i = 0; i < cardReverse.length; i++) {
+        let numero = parseInt(cardReverse[i], 10);
+
+        if (i % 2 != 0) {
+          let mult2 = numero * 2;
+          if (mult2 >= 10) {
+            let splitNum = splitToDigit(mult2);
+            //console.log(splitNum);
+            // console.log(splitNum.reduce((a, b) => a + b));
+            let sumNum = splitNum.reduce((a, b) => a + b);
+            //console.log("meto numero multiplicado por dos y sumado "+ sumNum);
+
+            result.push(sumNum);
+          } else {
+            //console.log("meto numero multiplicado por dos "+ mult2);
+
+            result.push(mult2);
+          }
+        } else {
+          //console.log("meto numero normal "+ numero);
+
+          result.push(numero);
+        }
+      }
+
+      //console.log(result);
+      let sumTotal = result.reduce((a, b) => a + b);
+     console.log(sumTotal);
+     console.log(sumTotal % 10 == 0);
+      return sumTotal % 10 == 0;
+    } else {
+      alert("ingresa los 16 digitos de tu tarjeta");
+    }
+  },
+  maskify(ccNumber){
+    let mask ="";
+    let cont= 0;
+    for(let i= ccNumber.length; i >= 0; i--) {
+      
+      if( cont<=4){
+        //console.log(ccNumber.charAt(i));
+        mask += ccNumber.charAt(i);
+      }else{
+        
+        console.log("#");
+        mask += "#";
+      }
+      cont++;
+    }
+    return reverse(mask);
+    
+  
+  } 
 };
+
+function splitToDigit(n) {
+  return (n + "").split("").map((i) => {
+    return Number(i);
+  });
+}
+
+function reverse(cc) {
+  return cc.split("").reverse().join("");
+}
 
 export default validator;
