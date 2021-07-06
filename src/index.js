77ef7108a431cd931354803e5c7f76a860d0ae7d
Victoria Tejeda <vicyatm@gmail.com>
2021-07-05 19:38:33 -0500

    Si es valido se muestra el numero enmascarado en el input, agrego boton reset, agrego imagen

--- src/index.js
+++ src/index.js
@@ -10,15 +10,21 @@ window.validaNum = function validaNum(event){
 window.validaBtn = function validaBtn(){
     let ccNumber= document.getElementById("CardNum").value;
     if(ccNumber.length ==16){
-        validator.maskify(ccNumber);
-        validator.isValid(ccNumber);
         if (validator.isValid(ccNumber)){
-            document.getElementById("mask").innerHTML = "El número de tu tarjeta es: " + validator.maskify(ccNumber);
+            document.getElementById("CardNum").value= validator.maskify(ccNumber);
+            document.getElementById("h1-valid").innerHTML = "aquí va un icono verde";
         }else {
             alert("ingresa un número valido");
+            document.getElementById("h1-valid").innerHTML = "aquí va un icono rojo";
         }
     }else{
         alert("ingresa los 16 digitos de tu tarjeta");
+        document.getElementById("h1-valid").innerHTML = "aquí va un icono rojo";
+
     }
 }
 
+window.reset= function reset(){
+    document.getElementById("CardNum").value="";
+    
+}
